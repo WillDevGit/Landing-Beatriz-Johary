635b2b1a8c49229884370e4a32740f92f54eff28
WillDevGit <williansilvaws063@gmail.com>
2026-08-30 23:43:46 -0300

    fix: improve clients section responsiveness

--- src/app/_components/clients.tsx
+++ src/app/_components/clients.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import Image from "next/image";
+
 import Bayer_logo from "../../../public/brands/bayer_brand.png";
 import cocacola_logo from "../../../public/brands/cocacola_brand.png";
 import itau_logo from "../../../public/brands/itau_brand.png";
@@ -9,67 +11,37 @@ import oboticario_logo from "../../../public/brands/oboticario_brand.png";
 import natura_logo from "../../../public/brands/natura_brand.png";
 import globo_logo from "../../../public/brands/globo_brand.png";
 
+const clients = [
+  { src: Bayer_logo, alt: "Bayer" },
+  { src: cocacola_logo, alt: "Coca-Cola" },
+  { src: natura_logo, alt: "Natura" },
+  { src: oboticario_logo, alt: "O Boticário" },
+  { src: itau_logo, alt: "Itaú" },
+  { src: sebrae_logo, alt: "Sebrae" },
+  { src: redbull_logo, alt: "Red Bull" },
+  { src: globo_logo, alt: "Globo" },
+];
+
 export function Clients() {
   return (
-    <section className="text-red-700 font-montserrat">
-      <div className="flex gap-2 items-center justify-between uppercase font-bold tracking-tighter">
-        <p>Colaborações</p>
-        <hr className="block flex-1 border-t mx-2 border-red-800" />
+    <section className="font-montserrat text-red-700">
+      <div className="flex items-center gap-2 uppercase font-bold tracking-tighter">
+        <p className="whitespace-nowrap">Colaborações</p>
+
+        <hr className="flex-1 border-t border-red-800" />
       </div>
 
-      <div className="grid grid-cols-4  gap-y-4 pt-2">
-        <div className="relative w-[50px] h-[50px]">
-          <Image src={Bayer_logo} alt="Bayer" fill className="object-contain" />
-        </div>
-        <div className="relative w-[60px] h-[50px]">
-          <Image
-            src={cocacola_logo}
-            alt="coca cola"
-            fill
-            className="object-contain"
-          />
-        </div>
-        <div className="relative w-[60px] h-[50px]">
-          <Image
-            src={natura_logo}
-            alt="natura"
-            fill
-            className="object-contain"
-          />
-        </div>
-        <div className="relative w-[60px] h-[50px]">
-          <Image
-            src={oboticario_logo}
-            fill
-            alt="oBoticario"
-            className="object-contain"
-          />
-        </div>
-        <div className="relative w-[50px] h-[60px]">
-          <Image src={itau_logo} alt="itau" fill className="object-contain" />
-        </div>
-
-        <div className="relative w-[50px] h-[60px]">
-          <Image
-            src={sebrae_logo}
-            alt="sebrae"
-            fill
-            className="object-contain"
-          />
-        </div>
-
-        <div className="relative w-[70px] h-[60px]">
-          <Image
-            src={redbull_logo}
-            alt="red bull"
-            fill
-            className="object-contain"
-          />
-        </div>
-
-        <div className="relative w-[60px] h-[60px]">
-          <Image src={globo_logo} alt="globo" fill className="object-contain" />
-        </div>
+      <div className="grid grid-cols-4 md:grid-cols-8 gap-4 md:gap-6 pt-6">
+        {clients.map((client) => (
+          <div key={client.alt} className="relative w-full aspect-square">
+            <Image
+              src={client.src}
+              alt={client.alt}
+              fill
+              className="object-contain"
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
