e933d8a7c775b7ee1a8c0be8f87953d4a518b6fe
WillDevGit <williansilvaws063@gmail.com>
2026-09-07 19:36:28 -0300

    feat: add design to menu navbar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,6 +1,10 @@
 "use client";
 import Image from "next/image";
 import menu from "../../../public/menu.png";
+import close from "../../../public/close.png";
+import linkedln_logo from "../../../public/linkedin.png";
+import insta_logo from "../../../public/instagram.png";
+import seta from "../../../public/arrow-white.png";
 import { useEffect, useState } from "react";
 
 export function NavBar() {
@@ -50,38 +54,68 @@ export function NavBar() {
       </nav>
       {isOpenMenu && (
         <div
-          className={`fixed inset-0 z-50 overflow-hidden grid grid-rows-3 uppercase px-6 pt-2 bg-red-900  transition-all duration-300 ease-in-out
-    ${isOpenMenu ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"}`}
+          className={`fixed flex flex-col inset-0 z-50 overflow-hidden uppercase px-6 pt-5 bg-red-900 transition-all duration-300 ease-in-out
+    ${isOpenMenu ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"} text-white `}
         >
-          <div className="flex justify-between block font-medium">
-            <h1 className="font-montserrat">
+          <button onClick={handleClickMenu} className="absolute right-8 top-6">
+            <Image src={close} alt="Fechar" />
+          </button>
+          <div className="flex justify-between block font-medium ">
+            <h1 className="font-bebas leading-6 text-3xl">
               Beatriz <br></br> Johary
             </h1>
-            <p onClick={handleClickMenu} className="text-2xl font-medium ">
-              X
-            </p>
           </div>
-          <div className="flex flex-col gap-6 font-medium">
+          <div className="flex flex-col gap-6 text-2xl mt-16 font-montserrat">
             <div className="cursor-pointer">
+              <div className="flex items-center">
+                <p className="opacity-60">01</p>
+                <hr className="block w-10 border-t mx-3 opacity-60" />
+              </div>
+
               <div className="pb-2">Sobre</div>
-              <hr className="mx-1 opacity-40" />
             </div>
 
             <div className="cursor-pointer">
+              <div className="flex items-center">
+                <p className="opacity-60">02</p>
+                <hr className="block w-10 border-t mx-3 opacity-60" />
+              </div>
+
               <div className="pb-2">Portifólio</div>
-              <hr className="mx-1 opacity-40" />
             </div>
+
             <div className="cursor-pointer">
+              <div className="flex items-center">
+                <p className="opacity-60">03</p>
+                <hr className="block w-10 border-t mx-3 opacity-60" />
+              </div>
+
               <div className="pb-2">Experiência</div>
-              <hr className="mx-1 opacity-40" />
             </div>
             <div className="cursor-pointer">
+              <div className="flex items-center">
+                <p className="opacity-60">04</p>
+                <hr className="block w-10 border-t mx-3 opacity-60" />
+              </div>
               <div className="pb-2">Contato</div>
-              <hr className="mx-1 opacity-40" />
             </div>
           </div>
-          <div className="border-2 border-white">
-            <div>Redes Sociais</div>
+          <hr className="my-6 opacity-60" />
+          <div className="">
+            <div className="flex items-center gap-18">
+              <button className="text-sm font-semibold font-montserrat uppercase">
+                Vamos conversar ?
+              </button>
+              <Image src={seta} alt="instagram" className="h-6 w-6" />
+            </div>
+            <div className="flex gap-6 pt-4">
+              <div className="border border-white p-2 rounded-full">
+                <Image src={linkedln_logo} alt="linkedln" className="h-4 w-4" />
+              </div>
+              <div className="border border-white p-2 rounded-full">
+                <Image src={insta_logo} alt="instagram" className="h-4 w-4" />
+              </div>
+            </div>
           </div>
         </div>
       )}
